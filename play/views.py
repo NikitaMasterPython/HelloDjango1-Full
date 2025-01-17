e386a745c4ce48682d47700ba6794e3da8261ea4
nikita <nikitagooogl@gmail.com>
2025-01-18 00:28:16 +1100

    start2

--- play/views.py
+++ play/views.py
@@ -1,5 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-def play(request):
-    return render(request, 'main/play.html')
+# def play(request):
+#     return render(request, 'main/play.html')
+#
+# def index(request):
+#     return render(request, 'main/index.html')
