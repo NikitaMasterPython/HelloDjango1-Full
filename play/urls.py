638d9b37dbb1ac3ed4e2249f9cfdde003b4612af
nikita <nikitagooogl@gmail.com>
2025-04-12 20:08:10 +1100

    Start consequence_1

--- play/urls.py
+++ play/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
     path('play/', views.play, name='play'),
     path('event/<int:event_id>/', views.play_next, name='play_next'),
     path('event_end/<int:event_id>/', views.event_part_2, name='event_part_2'),
+    path('random_event/', views.random_event, name='random_event'),
+    path('consequence/<int:event_id>/<int:action>/', views.get_consequence, name='get_consequence'),
 
 ]
--- play/views.py
+++ play/views.py
@@ -1,23 +1,61 @@
 from django.http import HttpResponse
+from django.http import JsonResponse
 from django.shortcuts import render, redirect
 from django.shortcuts import render, get_object_or_404
 from .models import event
+import random
 
 
 # from django.http import  JsonResponse
+def get_random_event_id():
+    """Вспомогательная функция для получения случайного ID события"""
+    event_ids = list(event.objects.values_list('id', flat=True))
+    return random.choice(event_ids) if event_ids else None
 
+def play(request):
+    """Перенаправление на случайное событие"""
+    random_id = get_random_event_id()
+    if random_id:
+        return redirect('play_next', event_id=random_id)
+    return render(request, 'play/play.html')
 
-
-def play (request):
-
-    return render (request, 'play/play.html')
-
-def play_next (request, event_id):
+def play_next(request, event_id):
+    """Обработка события"""
     Event = get_object_or_404(event, id=event_id)
-    return render(request,'play/play.html', {'event': Event})
+    context = {
+        'event': Event,
+        'random_event_id': get_random_event_id()  # Передаем случайный ID в шаблон
+    }
+    return render(request, 'play/play.html', context)
 
 def event_part_2 (request, event_id):
     Event = get_object_or_404(event, id=event_id)
     return render(request,'play/play.html', {'event': Event})
+
+def random_event(request):
+    random_id = get_random_event_id()
+    if random_id:
+        return redirect('play_next', event_id=random_id)
+    return redirect('play')
+
+
+def get_consequence(request):
+    event_id = request.GET.get('event_id')
+    action = request.GET.get('action')
+
+    try:
+        event_obj = event.objects.get(id=event_id)
+        consequence = event_obj.consequence_1 if action == '1' else event_obj.consequence_2
+        return JsonResponse({
+            'consequence': consequence,
+            'success': True
+        })
+    except event.DoesNotExist:
+        return JsonResponse({'success': False}, status=404)
+
+
+
+
+
 
 
